2e870b16f9a50f0ac8c7c0c394e09b26e8a9ba20
dduuukk <bender.c@northeastern.edu>
2024-03-17 20:46:02 -0400

    Updated to include SAI1 clk and pin init #4

--- src/drivers/sai-driver.cpp
+++ src/drivers/sai-driver.cpp
@@ -3,25 +3,50 @@
 // Initializer
 SAIDriver::SAIDriver(SAI_HandleTypeDef *hsai) {
     // Perform any necessary initialization for SAI peripheral
-    HAL_SAI_MspInit(SAI_HandleTypeDef *hsai);
+    HAL_SAI_MspInit(hsai);
+}
+
+// Clock source init
+void SAIDriver::initClk() {
+    // Enable all possible necessary clocks possible for daisy
+    // Enable SAI interface clocks
+    __HAL_RCC_SAI1_CLK_ENABLE();
+
+    // Enable DMA clocks
+    // NOTE: NOT SURE THIS IS NECESSARY RIGHT NOW, WILL BE IN FUTURE
+    __HAL_RCC_DMA1_CLK_ENABLE();
+
+    // Enable clocks for SAI GPIOs
+    // TODO: DEPENDING ON WHERE WE INPUT/OUTPUT FOR TESTING CHANGE THIS
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOE_CLK_ENABLE();
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+}
+
+// SAI pins init
+void SAIDriver::initPins() {
+    // Initialize pins for SAI1
+    GPIO_Config.Pin = SAIDriver::gpioPinValues::SAI1_FS_A |
+                      SAIDriver::gpioPinValues::SAI1_MCLK_A |
+                      SAIDriver::gpioPinValues::SAI1_SCK_A |
+                      SAIDriver::gpioPinValues::SAI1_SA_A |
+                      SAIDriver::gpioPinValues::SAI1_SB_A;
+    // Configure SAI pins as alternate function pull-up.
+    GPIO_Config.Mode = GPIO_MODE_AF_PP;
+    GPIO_Config.Pull = GPIO_PULLUP;
+    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH; // TODO: CHECK OVER SPEEDS, SEE IF MEDIUM IS OK
+    GPIO_Config.Alternate = GPIO_AF6_SAI1;
+    HAL_GPIO_Init(GPIOE, &GPIO_Config);
 }
 
 // HAL SAI MSP Init
 void SAIDriver::HAL_SAI_MspInit(SAI_HandleTypeDef* hsai) {
     // Perform any necessary initialization for SAI peripheral
-    if (hsai->Instance == SAI1_Block_A) {
-        // Enable SAI interface clock
-        __HAL_RCC_SAI1_CLK_ENABLE();
-
-
-        // __HAL_RCC_SAI1_CLK_ENABLE();
+    if (hsai->Instance == SAI1_Block_A || hsai->Instance == SAI1_Block_B) {
+        // Enable clocks
+        initClk();
 
         // Configure SAI pins
-        // GPIO_Config.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
-        // GPIO_Config.Mode = GPIO_MODE_AF_PP;
-        // GPIO_Config.Pull = GPIO_NOPULL;
-        // GPIO_Config.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        // GPIO_Config.Alternate = GPIO_AF6_SAI1;
-        // HAL_GPIO_Init(GPIOE, &GPIO_Config);
+        initPins();
     }
 }
--- src/drivers/sai-driver.h
+++ src/drivers/sai-driver.h
@@ -14,33 +14,32 @@ class SAIDriver {
         void HAL_SAI_MspInit(SAI_HandleTypeDef* hsai);  
         void clocks_initialise(void);
         void initGPIO(void);
-        // {
-        //     // Perform any necessary initialization for SAI peripheral
-        //     if (hsai->Instance == /* your SAI peripheral instance */) {
-        //         // Enable SAI clock
-        //         /* your code here */
 
-        //         // Configure SAI pins
-        //         /* your code here */
-
-        //         // Configure SAI interrupts
-        //         /* your code here */
-        //     }
-        // }
     private:
-        RCC_ClkInitTypeDef rccClkInstance = {
-            .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
-                        RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
-            .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
-            .AHBCLKDivider = RCC_SYSCLK_DIV1,
-            .APB1CLKDivider = RCC_HCLK_DIV1,
-            .APB2CLKDivider = RCC_HCLK_DIV1,
+        // GPIO Pin values accoring to the daisy documentation
+        enum gpioPinValues {
+            SAI1_SA_A = GPIO_PIN_6,
+            SAI1_SB_A = GPIO_PIN_3,
+            SAI1_FS_A = GPIO_PIN_4,
+            SAI1_SCK_A = GPIO_PIN_5,
+            SAI1_MCLK_A = GPIO_PIN_2
         };
-        RCC_OscInitTypeDef clock_setup = {
-            .OscillatorType = RCC_OSCILLATORTYPE_HSE,
-            .HSEState = RCC_CR_HSEON,
-            .PLL = {.PLLState = RCC_PLL_ON, .PLLSource = RCC_PLLSOURCE_HSE}
-        };
+
+        void initPins();
+        void initClk();
+        // RCC_ClkInitTypeDef rccClkInstance = {
+        //     .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
+        //                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
+        //     .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
+        //     .AHBCLKDivider = RCC_SYSCLK_DIV1,
+        //     .APB1CLKDivider = RCC_HCLK_DIV1,
+        //     .APB2CLKDivider = RCC_HCLK_DIV1,
+        // };
+        // RCC_OscInitTypeDef clock_setup = {
+        //     .OscillatorType = RCC_OSCILLATORTYPE_HSE,
+        //     .HSEState = RCC_CR_HSEON,
+        //     .PLL = {.PLLState = RCC_PLL_ON, .PLLSource = RCC_PLLSOURCE_HSE}
+        // };
         GPIO_InitTypeDef GPIO_Config;
 };
 
