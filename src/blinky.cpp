4a01c7a9a75e33f837d4203b2aa1ea643a9cdba1
nmitchell <mitchell.no@northeastern.edu>
2024-04-01 12:00:35 -0400

    #14 oops

--- src/blinky.cpp
+++ src/blinky.cpp
@@ -5,20 +5,6 @@
 #define LED_PIN GPIO_PIN_0
 #define LED_PORT_CLK_ENABLE __HAL_RCC_GPIOC_CLK_ENABLE
 
-static RCC_ClkInitTypeDef rccClkInstance = {
-    .ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK |
-                 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2,
-    .SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK,
-    .AHBCLKDivider = RCC_SYSCLK_DIV1,
-    .APB1CLKDivider = RCC_HCLK_DIV1,
-    .APB2CLKDivider = RCC_HCLK_DIV1,
-};
-
-static RCC_OscInitTypeDef clock_setup = {
-    .OscillatorType = RCC_OSCILLATORTYPE_HSE,
-    .HSEState = RCC_CR_HSEON,
-    .PLL = {.PLLState = RCC_PLL_ON, .PLLSource = RCC_PLLSOURCE_HSE}};
-
 void clocks_initialise(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
