09fd564c999f50234179da93c740e2e9298ac921
cole-ch <cole.ch@northeastern.edu>
2024-04-22 18:17:55 -0400

    more storange im scared

--- src/per/UART.h
+++ src/per/UART.h
@@ -12,32 +12,6 @@ UART_HandleTypeDef huart1;
 
 uint8_t rx_buff[8];
 
-void HAL_UART_MspInit(UART_HandleTypeDef* huart)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(huart->Instance==USART1)
-  {
-    __HAL_RCC_USART1_CLK_ENABLE();
-    
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**I2C2 GPIO Configuration
-    PB6      ------> USART1_TX
-    PB7      ------> USART1_RX
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_6;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_7;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_INPUT;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-    /* USART1 interrupt Init */
-    HAL_NVIC_SetPriority(USART1_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(USART1_IRQn);
-  }
-}
+void HAL_UART_MspInit(UART_HandleTypeDef* huart);
 
 static void initUART1(void);
